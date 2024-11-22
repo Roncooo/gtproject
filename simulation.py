3f9d880d6306d8859731462bcb802246a9e4d685
Roncooo <francescooroncolatoo@gmail.com>
2024-11-22 15:24:54 +0100

    Completely rewrote game rules. Now it seems P1 is hugely in disadvantage. Could be a bug of the code or a big feature for the paper

--- simulation.py
+++ simulation.py
@@ -1,115 +1,68 @@
 import numpy as np
 import sympy as sp
 import random
-import itertools
-import operator
-import time
 from prettytable import PrettyTable 
 
 max_card = 25
 num_card = 24
+composite_score = 1
+prime_score = 2
 
-import warnings
-# Suppress some numerical useless errors 
-warnings.filterwarnings("ignore", category=RuntimeWarning, message=".*divide by zero.*")
-warnings.filterwarnings("ignore", category=RuntimeWarning, message=".*invalid value encountered in scalar divide.*")
-warnings.filterwarnings("ignore", category=RuntimeWarning, message=".*invalid value encountered in scalar multiply.*")
+def is_prime_index(index):
+    # knowing that gameboard is built like this
+    # [primes p1, composites p1, primes p2, composites p2]
+    # we can easily tell if in a position there is a prime or not
+    return index%2==0
+
+# tells if operand1 and operand2 can give result with the admitted operations
+# this function automatically checks all the possible order of operands
+def is_valid_operation(result, operand1, operand2):
+    if result==operand1+operand2:
+        return True
+    if result==operand1-operand2 or result==operand2-operand1:
+        return True
+    if result==operand1*operand2:
+        return True
+    if operand2!=0 and result==operand1/operand2:
+        return True
+    if operand1!=0 and result==operand2/operand1:
+        return True
+    return False
 
 
-# Returns, if it exists, the tuple like (num op num op num) that gives as output result, otherwise []
-# It tries all the possible permutations of numbers and operations
-# In our game we are happy if we find one expression with 3 numbers that gives the result. If we find it,
-# it means that we used all the possible numbers, thus getting the maximum score in that case
-# So we do not care about different permutations of the 3 operands or operations
-def find_combination_of_3(numbers, result):
-    if len(numbers) != 3:
-        raise ValueError("The function supports exactly 3 numbers.")
+def best_score(gb, result_index):    
+    # if we have a prime result then we can have at best an operation with one prime and one composite: 3 points
+    # if we have a composite result then we can have at best an operation with two primes: 4 points
+    if is_prime_index(result_index):
+        max_operation_score = prime_score + composite_score
+        placed_card_score = prime_score
+    else:
+        max_operation_score = 2 * prime_score
+        placed_card_score = composite_score
+    best_operation_score = 0 # best score found so far
     
-    # Define the operations
-    operations = [operator.add, operator.sub, operator.mul, operator.truediv]
-    operation_symbols = ['+', '-', '*', '/']
-    
-    # Generate all permutations of numbers
-    for num_order in itertools.permutations(numbers):
-        # Generate all combinations of operations
-        for op1_index, op1 in enumerate(operations):
-            for op2_index, op2 in enumerate(operations):
-                try:
-                    # Compute results with all combinations of numbers and operations
-                    res1 = op1(num_order[0], num_order[1])
-                    res2 = op2(res1, num_order[2])
-                    if res2==result:
-                        return (num_order[0], 
-                                    operation_symbols[op1_index], 
-                                    num_order[1],
-                                    operation_symbols[op2_index], 
-                                    num_order[2], 
-                                    res2)
-                except ZeroDivisionError or ValueError:
-                    continue
-    return []
-
-# Returns, if existing, an array of all the possible tuples (num op num = result), otherwise []
-# In the case of operations with two cards, we do care about all possible combinations, because some
-# of them may have a different number of primes and composites, and so different score
-def find_combinations_of_2(numbers):
-    if len(numbers) != 2:
-        raise ValueError("The function supports exactly 2 numbers.")
-    
-    # Define the operations
-    operations = [operator.add, operator.sub, operator.mul, operator.truediv]
-    operation_symbols = ['+', '-', '*', '/']
-    
-    results = []
-    
-    # Generate all permutations of numbers
-    for num_order in itertools.permutations(numbers):
-        # Generate all combinations of operations
-        for op_index, op in enumerate(operations):
-            try:
-                # Compute results with all combinations of numbers and operations
-                res = op(num_order[0], num_order[1])
-                results.append((num_order[0], 
-                                operation_symbols[op_index], 
-                                num_order[1],
-                                res))
-            except ZeroDivisionError or ValueError:
+    # this nested loop chooses the couples of operands
+    # the order does not matter since is_valid_operation deals with it
+    for i in range(4):
+        if i == result_index:
+            continue
+        for j in range(i+1, 4):
+            if not is_valid_operation(result=gb[result_index], operand1=gb[i], operand2=gb[j]):
                 continue
-    return results
-
-def score(comb):
-    score = 0
-    prime_points = 2
-    composite_points = 1
-    # this works both for comb2 and comb3
-    for n in comb[0::2]:
-        if sp.isprime(n):
-            score += prime_points
-        else:
-            score += composite_points
-    return score
-
-def best_score(gb, result_index):
-    cards = np.delete(gb, result_index)
-    result = gb[result_index]
+            
+            score_card_i = prime_score if is_prime_index(i) else composite_score
+            score_card_j = prime_score if is_prime_index(j) else composite_score
+            score_of_this_couple = score_card_i + score_card_j
+            
+            if score_of_this_couple > best_operation_score:
+                best_operation_score = score_of_this_couple
+            
+            if max_operation_score == best_operation_score:
+                return best_operation_score + placed_card_score # early stop
     
-    comb3 = find_combination_of_3(cards, result)
-    if comb3!=[]:
-        # in this case the score is unique because the computation uses all the three remaining cards
-        return score(comb3)
-          
-    comb2 = (find_combinations_of_2((cards[0], cards[1])) + 
-             find_combinations_of_2((cards[0], cards[2])) + 
-             find_combinations_of_2((cards[1], cards[2])) )
-    # only the combinations that have as result the wanted number
-    valid_comb2 = [comb for comb in comb2 if comb[3]==result]
-    if len(valid_comb2)==0:
-        # no combinations of 3 nor of 2 found -> 0 points
-        return 0
+    # if no valid operation is found, it is just placed_card_score so 1 or 2
+    return best_operation_score + placed_card_score
     
-    # there is at least one combination of two that gives result 
-    return max([score(comb) for comb in valid_comb2])
-
 
 # Policies can be 'asc', 'desc', 'rand' (todo, add other policies, ex. primes first etc)
 # Those are stupid policies, the player does not look at the cards on the table to do his move
@@ -120,6 +73,12 @@ def play_one_game(policy1, policy2):
 
     player1 = deck[:cards_per_player]
     player2 = deck[ cards_per_player:]
+    
+    # player1 is the first to play: according to the rules, he must have 2 in his deck
+    # if this is not the case i switch the decks
+    if 2 not in player1:
+        player1, player2 = player2, player1
+    
     score1 = 0
     score2 = 0
 
@@ -168,7 +127,6 @@ def play_one_game(policy1, policy2):
 def play_n_games(policy1, policy2, n_games=1000):
     win1 = 0
     ties = 0
-    start_time = time.time()
     for i in range(n_games):
         score1, score2 = play_one_game(policy1, policy2)
         if score1 > score2:
@@ -176,7 +134,6 @@ def play_n_games(policy1, policy2, n_games=1000):
         if score1 == score2:
             ties +=1
     win2 = n_games-ties-win1
-    end_time = time.time()
     win1/=n_games
     win2/=n_games
     ties/=n_games
@@ -189,7 +146,8 @@ myTable = PrettyTable(["P1\\P2"] + policies)
 for p1 in policies:
     row = [p1]
     for p2 in policies:
-        row += [play_n_games(p1, p2, n_games=1000)]
+        row += [play_n_games(p1, p2, n_games=100000)]
     myTable.add_row(row)
 
+print("For each cell, win rate p1 | tie rate | win rate p2")
 print(myTable)
