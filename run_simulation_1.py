f6c1e41471540d641881f0fd28b9b2b099979138
Roncooo <francescooroncolatoo@gmail.com>
2025-01-03 17:46:57 +0100

    Parallelized simulations

--- run_simulation_1.py
+++ run_simulation_1.py
@@ -2,13 +2,14 @@ from Primi_composti_1.play_primi_composti_1 import *
 from utilities.simulations import play_n_games_for_each_policy_combination
 from utilities.utils import print_results
 import time
+import os
 
 if __name__ == "__main__":
-    n_games = 1 # remember to disable all the prints in play_one_game to speed up the process (a lot)
+    n_games = 10 # remember to disable all the prints in play_one_game to speed up the process (a lot)
     policies = ['asc', 'minimax_4']
     
     start = time.time()
-    results = play_n_games_for_each_policy_combination(n_games = n_games, policies=policies, play_one_game_function = play_one_game_1, print_game_function = print_game_1, seed=None, log_game=False)
+    results = play_n_games_for_each_policy_combination(n_games = n_games, policies=policies, play_one_game_function = play_one_game_1, print_game_function = print_game_1, seed=None, log_game=False, max_n_processes=os.cpu_count())
     end = time.time()
     
     print_results(results, policies, True)
--- utilities/simulations.py
+++ utilities/simulations.py
@@ -1,5 +1,7 @@
 from utilities.policies import *
 import numpy as np
+from multiprocessing import Pool
+
 
 def sort_deck_according_to_policy(policy, player_deck):
     ''' Returns the array of cards sorted according to a policy '''
@@ -10,15 +12,23 @@ def sort_deck_according_to_policy(policy, player_deck):
         case 'greedy_desc': return np.sort(player_deck)[::-1]
         case _: return player_deck
 
-def play_n_games(policy1, policy2, n_games, play_one_game_function, print_game_function, seed=None, log_game=False ):
+def play_n_games(policy1, policy2, n_games, play_one_game_function, print_game_function, seed=None, log_game=False, max_n_processes=1):
+    '''Plays `n_games` times a match with `policy_1` vs `policy_2`. If `n_games` is greater than 1 then `seed` is ignored (otherwise the games would be identical). This function parallelizes the execution of the different matches, creating one process for each game (up to `max_n_processes`).'''
+    
+    if n_games>1:
+        seed = None
+    
     win_count_p1 = 0
     tie_count = 0
     tot_score1 = 0
     tot_score2 = 0
     abs_score_diff = 0
 
-    for _ in range(n_games):
-        score1, score2, deck_p1, deck_p2 = play_one_game_function(policy1, policy2, seed)
+    # This context manager automatically handles the joining of processes once the block of code is completed (so no need to explicitly wait for the processes to end)
+    with Pool(processes=max_n_processes) as pool:
+        results = pool.starmap(play_one_game_function, [(policy1, policy2, seed) for _ in range(n_games)])
+
+    for score1, score2, deck_p1, deck_p2 in results:
         if score1 > score2:
             win_count_p1 += 1
         if score1 == score2:
@@ -39,12 +49,12 @@ def play_n_games(policy1, policy2, n_games, play_one_game_function, print_game_f
     avg_abs_score_diff = abs_score_diff / n_games
     return winrate_p1, avg_score_1, tierate, winrate_p2, avg_score_2, avg_abs_score_diff
 
-def play_n_games_for_each_policy_combination(play_one_game_function, print_game_function, n_games=1000, policies=SIMPLE_POLICIES, seed=None, log_game=False):
+def play_n_games_for_each_policy_combination(play_one_game_function, print_game_function, n_games=1000, policies=SIMPLE_POLICIES, seed=None, log_game=False, max_n_processes=1):
     results = []
     for p1 in policies:
         row = []
         for p2 in policies:
-            winrate_p1, avg_score_1, tierate, winrate_p2, avg_score_2, avg_abs_score_diff = play_n_games(p1, p2, n_games,play_one_game_function, print_game_function, seed, log_game)
+            winrate_p1, avg_score_1, tierate, winrate_p2, avg_score_2, avg_abs_score_diff = play_n_games(p1, p2, n_games,play_one_game_function, print_game_function, seed, log_game, max_n_processes)
             row += [[winrate_p1, avg_score_1, tierate, winrate_p2, avg_score_2, avg_abs_score_diff]]
         results += [row]
     return results
