16d1933294ff3031458e3178023555451ab42b31
boorgoof <federico.31mene@icloud.com>
2025-01-09 08:40:06 +0100

    minimax 5 vs minimax6

--- run_simulation_2.py
+++ run_simulation_2.py
@@ -6,8 +6,8 @@ import time
 import os
 
 if __name__ == "__main__":
-    n_games = 1000 # remember to disable all the prints in play_one_game to speed up the process (a lot)
-    policies = ['greedy_asc','greedy_desc','greedy_rand','minimax_4', 'minimax_5']
+    n_games = 80 # remember to disable all the prints in play_one_game to speed up the process (a lot)
+    policies = ['minimax_5', 'minimax_6']
     max_n_processes = os.cpu_count()
     print(f"Starting to play {n_games} of game 2 for each combination of {policies} with {max_n_processes} parallel processes")
     
